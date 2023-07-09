f0ffa5cee5728fd2e6d911ebcd10041eca8f4f68
Manz2 <erikmanzer21@gmail.com>
2023-07-09 13:23:24 +0200

    remove secrets

--- lib/secrets.dart
+++
@@ -1,4 +0,0 @@
-const firebaseWebApiKey = "AIzaSyAruiq6gZ-GmCQ7gHclg3NnefuffVn6k9Q";
-const firebaseAndroidApiKey = "AIzaSyC-OWVpkxZCy_OmsJNa1euDbLx_i0FyL8M";
-const firebaseIOSApiKey = "AIzaSyArCp9H5ztS3eJ1jposEUHRnfuO9lmKujY";
-const openAiKey = "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";
