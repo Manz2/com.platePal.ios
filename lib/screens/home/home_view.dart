fbe53dd78958323dbb94a2a262b8e07d2a775391
Manz2 <erikmanzer21@gmail.com>
2024-04-26 16:40:00 +0200

    Refine registration #22

--- lib/screens/home/home_view.dart
+++ lib/screens/home/home_view.dart
@@ -16,7 +16,6 @@ class HomeView extends ConsumerWidget {
         ref.read(providers.homeControllerProvider.notifier);
     final HomeModel model = ref.watch(providers.homeControllerProvider);
     TextEditingController? searchController;
-    final focusNode = FocusNode();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       if (!model.hasFetchedOnInit) {
         controller.getRecipes(FirebaseAuth.instance.currentUser!.uid);
